23051413937e0e0d9cc9c4a819c65f3f4667bc6d
sajjad <sajjadkheiri7@gmail.com>
2024-11-22 23:08:46 +0330

    Add services

--- Api-Examples/gRPC/gRPC.FirstSample/Program.cs
+++ Api-Examples/gRPC/gRPC.FirstSample/Program.cs
@@ -3,7 +3,11 @@ using gRPC.FirstSample.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddGrpc();
+builder.Services.AddGrpcReflection();
 
 var app = builder.Build();
+
+app.MapGrpcService<PersonGrpcService>();
+app.MapGrpcReflectionService();
 
 app.Run();
--- Api-Examples/gRPC/gRPC.FirstSample/Services/PersonGrpcService.cs
+++ Api-Examples/gRPC/gRPC.FirstSample/Services/PersonGrpcService.cs
@@ -1,8 +1,101 @@
+using Google.Protobuf.WellKnownTypes;
 using gRPC.FirstSample.Protos;
+using Grpc.Core;
+using Microsoft.VisualBasic;
 
 namespace gRPC.FirstSample.Services;
 
 public class PersonGrpcService : PersonService.PersonServiceBase
 {
-    
+    private List<PersonReply> _people =
+    [
+        new PersonReply{
+            ID = 1,
+            FirstName = "Sajjad",
+            LastName = "Kheiri"
+        },
+        new PersonReply{
+            ID = 2,
+            FirstName = "Amir",
+            LastName = "Daneshvar"
+        },
+
+            new PersonReply{
+            ID = 3,
+            FirstName = "Sadegh",
+            LastName = "Zolghadr"
+        },
+            new PersonReply{
+            ID = 4,
+            FirstName = "Mohsen",
+            LastName = "Maghare"
+        },
+    ];
+
+    public override async Task CreatePerson(IAsyncStreamReader<CreatePersonRequest> requestStream, IServerStreamWriter<PersonReply> responseStream, ServerCallContext context)
+    {
+        await foreach (var person in requestStream.ReadAllAsync())
+        {
+            var peopleCount = _people.Count();
+
+            var personReply = new PersonReply
+            {
+                ID = ++peopleCount,
+                FirstName = person.FirstName,
+                LastName = person.LastName
+            };
+
+            _people.Add(personReply);
+
+            await responseStream.WriteAsync(personReply);
+        }
+    }
+
+    public override async Task<Empty> DeletePerson(IAsyncStreamReader<PersonByIdRequest> requestStream, ServerCallContext context)
+    {
+        await foreach (var person in requestStream.ReadAllAsync())
+        {
+            var personDeleted = _people.Where(x => x.ID == person.ID).SingleOrDefault();
+
+            if (personDeleted != null)
+            {
+                _people.Remove(personDeleted);
+            }
+        }
+
+        return new Empty();
+    }
+
+    public override async Task GetAll(Empty request, IServerStreamWriter<PersonReply> responseStream, ServerCallContext context)
+    {
+        foreach (var person in _people)
+        {
+            await responseStream.WriteAsync(person);
+        }
+    }
+
+    public override async Task<PersonReply> GetPersonById(PersonByIdRequest request, ServerCallContext context)
+    {
+        var person = _people.Where(x => x.ID == request.ID).SingleOrDefault();
+
+        if (person != null)
+        {
+            return person;
+        }
+
+        throw new RpcException(new Status(StatusCode.Unavailable, $"Person with Id {request.ID} not found"));
+    }
+
+    public override async Task<Empty> UpdatePerson(UpdatePersonRequest request, ServerCallContext context)
+    {
+        var person = _people.Where(x => x.ID == request.ID).SingleOrDefault();
+
+        if (person != null)
+        {
+            person.FirstName = request.FirstName;
+            person.LastName = request.LastName;
+        }
+
+        return new Empty();
+    }
 }
